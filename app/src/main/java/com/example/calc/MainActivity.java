30c42fde79c38f725c7bd6763f9bed376db346de
Disha-Baghel <bagheldisha708@gmail.com>
2023-02-23 19:56:25 +0530

    added layout design

--- app/src/main/java/com/example/calc/MainActivity.java
+++ app/src/main/java/com/example/calc/MainActivity.java
@@ -3,8 +3,14 @@ package com.example.calc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, b16, b17, b18, b19, b20;
+
+    b1 = 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
