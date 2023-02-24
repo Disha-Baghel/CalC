0edb1ad4013601b0a45f66f1e297859b39184e45
Disha-Baghel <bagheldisha708@gmail.com>
2023-02-24 06:04:23 +0530

    changed layout design

--- app/src/main/java/com/example/calc/MainActivity.java
+++ app/src/main/java/com/example/calc/MainActivity.java
@@ -10,7 +10,6 @@ public class MainActivity extends AppCompatActivity {
 
     Button b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, b16, b17, b18, b19, b20;
 
-    b1 = 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
