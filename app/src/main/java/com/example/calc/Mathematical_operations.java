e13df9bf7dc71e65c72eac9f027f12209be25378
Disha-Baghel <bagheldisha708@gmail.com>
2023-04-19 16:39:36 +0530

    Added new files

--- app/src/main/java/com/example/calc/Mathematical_operations.java
+++ app/src/main/java/com/example/calc/Mathematical_operations.java
@@ -1,7 +1,9 @@
 package com.example.calc;
-
+import java.util.Stack;
 import androidx.appcompat.app.AppCompatActivity;
 
 public interface Mathematical_operations{
-
+    Stack<Integer> stack = new Stack<Integer>();
+    public void add();
+    public void subtract();
 }
