605e971423a7c679c285387221c6d2132e703753
Disha-Baghel <bagheldisha708@gmail.com>
2023-04-20 20:56:22 +0530

    Added new files

---
+++ app/src/main/java/com/example/calc/Operations.java
@@ -0,0 +1,14 @@
+package com.example.calc;
+
+public class Operations implements Mathematical_operations{
+
+    @Override
+    public void add() {
+        stack.push(1);
+    }
+
+    @Override
+    public void subtract() {
+
+    }
+}
