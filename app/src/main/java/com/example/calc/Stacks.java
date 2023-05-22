ca6757d475538c50934e27516b19251cbd4e0af8
Disha-Baghel <bagheldisha708@gmail.com>
2023-05-22 19:46:11 +0530

    added this class to convert infix expression entered by the user to postfix expression

--- app/src/main/java/com/example/calc/Stacks.java
+++ app/src/main/java/com/example/calc/Stacks.java
@@ -1,2 +1,37 @@
-package com.example.calc;public class Stacks {
+package com.example.calc;
+
+import java.util.Stack;
+
+public class Stacks {
+
+    Stack<Character> stack = new Stack<Character>();
+
+    public int precedence (char symbol) {
+
+        switch (symbol) {
+
+            case '+' :
+            case '-' : return 1;
+            case '/' :
+            case '*' : return 2;
+            case '^' : return 3;
+            default : return -1;
+
+        }
+    }
+
+    public void infix_to_postfix(String expression) {
+
+        String finalExpression = "";
+
+        for (int i = 0; i < expression.length(); i++) {
+            char symbol = expression.charAt(i);
+            int precedence = precedence(symbol);
+
+            if(precedence == -1) {
+                finalExpression = finalExpression + symbol;
+            }
+        }
+    }
+
 }
