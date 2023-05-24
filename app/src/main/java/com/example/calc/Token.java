b9b9e60aade82708c1ef1ba8c69d4167cbaf0720
Disha-Baghel <bagheldisha708@gmail.com>
2023-05-24 22:42:10 +0530

    created a class Token to convert the entered expression to various tokens. The tokens are futher characterised into value and operators

--- app/src/main/java/com/example/calc/Token.java
+++ app/src/main/java/com/example/calc/Token.java
@@ -1,2 +1,39 @@
-package com.example.calc;public class Token {
+package com.example.calc;
+
+public class Token {
+
+    public double operand;
+
+    public enum TokenType {
+        TOKEN_OPERAND,
+
+        TOKEN_ADD,
+        TOKEN_SUBTRACT,
+        TOKEN_MULTIPLY,
+        TOKEN_DIVIDE,
+        TOKEN_EXPONENT,
+
+        TOKEN_INVALID,
+        TOKEN_EOF
+    }
+
+    public TokenType type;
+
+    Token(double operand, TokenType type) {
+        this.operand = operand;
+        this.type = type;
+    }
+
+    public boolean isOperand() {
+        return type == TokenType.TOKEN_OPERAND;
+    }
+
+    public boolean isEOF() {
+        return type == TokenType.TOKEN_EOF;
+    }
+
+    public boolean isInvalid() {
+        return type == TokenType.TOKEN_INVALID;
+    }
+
 }
