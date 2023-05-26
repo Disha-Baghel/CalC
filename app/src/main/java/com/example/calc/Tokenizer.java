afa6aed6a9794cef21579c4f8d7b57951acab0e1
Disha-Baghel <bagheldisha708@gmail.com>
2023-05-26 23:37:11 +0530

    class to push the tokens into an ArrayList and convert perform appropriate operations

--- app/src/main/java/com/example/calc/Tokenizer.java
+++ app/src/main/java/com/example/calc/Tokenizer.java
@@ -1,2 +1,72 @@
-package com.example.calc;public class Tokenizer {
+package com.example.calc;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.Character;
+
+public class Tokenizer {
+
+    private ArrayList<Token> tokenList;
+
+    private int currentTokenIndex = 0;
+    private int index = 0;
+
+    public Tokenizer(String input) {
+        tokenList = tokenize(input);
+    }
+
+    public Token peek() {
+        return tokenList.get(currentTokenIndex);
+    }
+
+    public void advance() {
+        if (!peek().isEOF()) {
+            currentTokenIndex += 1;
+        }
+    }
+
+    public  Token nextToken() {
+        advance();
+        return peek();
+    }
+
+    private boolean isOperand(char ch) {
+        return ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^';
+    }
+
+    private ArrayList<Token> tokenize(String input) {
+
+        ArrayList<Token> tokenList = new ArrayList<>();
+
+        while(index < input.length()) {
+            char ch = input.charAt(index);
+            Token token = new Token(0, Token.TokenType.TOKEN_INVALID);
+            if(Character.isDigit(ch)) {
+                token = extractDigit(index);
+            } else if (isOperand(ch)) {
+                token = extractOperand(ch);
+            }
+            tokenList.add(token);
+            ++index;
+        }
+
+        tokenList.add(new Token(0, Token.TokenType.TOKEN_EOF));
+        return tokenList;
+    }
+
+    private Token extractOperand(char ch) {
+        switch (ch) {
+            case '+': return new Token(0, Token.TokenType.TOKEN_ADD);
+            case '-': return new Token(0, Token.TokenType.TOKEN_SUBTRACT);
+            case '/': return new Token(0, Token.TokenType.TOKEN_DIVIDE);
+            case '*': return new Token(0, Token.TokenType.TOKEN_MULTIPLY);
+            case '^': return new Token(0, Token.TokenType.TOKEN_EXPONENT);
+        }
+        return new Token(-1, Token.TokenType.TOKEN_INVALID);
+    }
+
+    private Token extractDigit(int index) {
+
+    }
+
 }
